734166c5b4dc3e261f65c5b0c21d727ecb5d5371
Rahoul Baruah <rahoulb@echodek.co>
2025-01-29 10:14:05 +0000

    Stupid real world problems

--- db/migrate/20250127160616_create_operations_tasks.rb
+++ db/migrate/20250127160616_create_operations_tasks.rb
@@ -1,12 +1,12 @@
-class CreateOperationsTasks < ActiveRecord::Migration[8.0]
+class CreateOperationsTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :operations_tasks do |t|
       t.string :type
       t.integer :status, default: 0, null: false
       t.string :state, null: false
       t.string :status_message, default: "", null: false
-      t.text :data, default: "{}"
-      t.text :results, default: "{}"
+      t.text :data
+      t.text :results
       t.boolean :background, default: false, null: false
       t.datetime :delete_at, null: false, index: true
       t.timestamps
