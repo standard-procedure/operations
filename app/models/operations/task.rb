18991b63d8491732f17e147ccfede7f5b764677d
Rahoul Baruah <rahoulb@echodek.co>
2025-01-31 11:53:20 +0000

    Ensure status_message never overflows

--- app/models/operations/task.rb
+++ app/models/operations/task.rb
@@ -9,18 +9,18 @@ module Operations
 
     def self.call(data = {})
       raise MissingInputsError, "Missing inputs: #{missing_inputs_from(data).join(", ")}" unless required_inputs_are_present_in?(data)
-      create!(state: initial_state).tap do |task|
+      create!(state: initial_state, status_message: "").tap do |task|
         task.send(:process_current_state, DataCarrier.new(data.merge(task: task)))
       end
     end
 
     def go_to(state, data = {}, message = nil)
-      update!(state: state, status_message: message || state.to_s)
+      update!(state: state, status_message: (message || state).to_s.truncate(240))
       process_current_state(data)
     end
 
-    def fail_with(message) = update! status: "failed", results: {failure_message: message.to_s}
+    def fail_with(message) = update! status: "failed", status_message: message.to_s.truncate(240), results: {failure_message: message.to_s}
 
-    def complete(results) = update!(status: "completed", results: results)
+    def complete(results) = update!(status: "completed", status_message: "completed", results: results)
   end
 end
