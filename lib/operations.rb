aec949536f546329740b72adacfe285a93eddddc
Rahoul Baruah <rahoulb@echodek.co>
2025-02-04 15:33:04 +0000

    Tidied up the exceptions

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,6 +2,11 @@ require "ostruct"
 
 module Operations
   class Error < StandardError
+    def initialize message, task = nil
+      super(message)
+      @task = task
+    end
+    attr_reader :task
   end
   require "operations/version"
   require "operations/engine"
