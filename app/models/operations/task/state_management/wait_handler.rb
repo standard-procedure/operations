a12868e7501032a667526cf338618ac6b78f38be
Rahoul Baruah <rahoulb@echodek.co>
2025-03-04 21:20:46 +0000

    Multiple conditions in wait handlers

--- app/models/operations/task/state_management/wait_handler.rb
+++ app/models/operations/task/state_management/wait_handler.rb
@@ -1,18 +1,27 @@
 class Operations::Task::StateManagement::WaitHandler
   def initialize name, &config
     @name = name.to_sym
-    @next_state = nil
-    @condition = nil
+    @conditions = []
+    @destinations = []
     instance_eval(&config)
+    puts "Configured"
   end
 
-  def condition(&condition) = @condition = condition
+  def condition(&condition) = @conditions << condition
 
-  def go_to(state) = @next_state = state
+  def go_to(state) = @destinations << state
 
   def call(task, data)
     raise Operations::CannotWaitInForeground.new("#{task.class} cannot wait in the foreground", task) unless task.background?
-    next_state = data.instance_eval(&@condition) ? @next_state : task.state
-    data.go_to(next_state)
+    puts "Searching"
+    condition = @conditions.find { |condition| data.instance_eval(&condition) }
+    if condition.nil?
+      puts "None"
+      data.go_to task.state
+    else
+      index = @conditions.index condition
+      puts "Found #{@destinations[index]}"
+      data.go_to @destinations[index]
+    end
   end
 end
