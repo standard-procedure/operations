06f9216436e4f270600a371273679d50d53dc138
Rahoul Baruah <rahoulb@echodek.co>
2025-06-08 14:30:12 +0000

    Urg

--- app/jobs/operations/agent/find_timeouts_job.rb
+++ app/jobs/operations/agent/find_timeouts_job.rb
@@ -1,5 +1,5 @@
 class Operations::Agent::FindTimeoutsJob < ApplicationJob
   queue_as :default
 
-  def perform = Operations::Agent.active.timed_out.find_each { |agent| Operations::TimeoutJob.perform_later(agent) }
+  def perform = Operations::Agent.active.timed_out.find_each { |agent| Operations::Agent::TimeoutJob.perform_later(agent) }
 end
