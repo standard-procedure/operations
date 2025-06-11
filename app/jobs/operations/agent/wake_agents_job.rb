8992684c1345f826d7b1a1830b479f869a8c1337
Rahoul Baruah <rahoulb@echodek.co>
2025-06-11 09:22:55 +0000

    Added debug code for deployment issues

--- app/jobs/operations/agent/wake_agents_job.rb
+++ app/jobs/operations/agent/wake_agents_job.rb
@@ -1,5 +1,10 @@
 class Operations::Agent::WakeAgentsJob < ApplicationJob
   queue_as :default
 
-  def perform = Operations::Agent.waiting.ready_to_wake.find_each { |agent| Operations::Agent::RunnerJob.perform_later(agent) }
+  def perform
+    Rails.logger.info { Operations::Task.waiting.pluck(:type, :state).map { |info| info.join(": ") }.join(", ") }
+    Operations::Agent.waiting.ready_to_wake.find_each do |agent|
+      Operations::Agent::RunnerJob.perform_later(agent)
+    end
+  end
 end
